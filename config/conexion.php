d92ea53ac5524e917e01618a1f047d7ae3cd381b
Romel Molina <romel.molina.est@tecazuay.edu.ec>
2026-07-30 01:38:35 -0500

    Probar variables Railway

--- config/conexion.php
+++ config/conexion.php
@@ -1,12 +1,3 @@
 <?php
-
-$servidor   = getenv("MYSQLHOST") ?: getenv("MYSQL_HOST");
-$usuario    = getenv("MYSQLUSER") ?: getenv("MYSQL_USER");
-$contrasena = getenv("MYSQLPASSWORD") ?: getenv("MYSQL_PASSWORD");
-$basedatos  = getenv("MYSQLDATABASE") ?: getenv("MYSQL_DATABASE");
-$puerto     = getenv("MYSQLPORT") ?: getenv("MYSQL_PORT");
-
-echo "<pre>";
-var_dump($servidor, $usuario, $basedatos, $puerto);
-echo "</pre>";
+var_dump(getenv("PRUEBA"));
 exit;
