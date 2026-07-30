dabbeaadd0242230cf4fce5ed37e16f005416725
Romel Molina <romel.molina.est@tecazuay.edu.ec>
2026-07-30 01:45:34 -0500

    Probar variables Railway

--- config/conexion.php
+++ config/conexion.php
@@ -1,7 +1,29 @@
 <?php
-echo "<pre>";
-print_r($_ENV);
-echo "\n====================\n";
-print_r($_SERVER);
-echo "</pre>";
-exit;
+
+$url = getenv("MYSQL_URL");
+
+if (!$url) {
+    die("MYSQL_URL no encontrada");
+}
+
+$partes = parse_url($url);
+
+$host = $partes["host"];
+$usuario = $partes["user"];
+$contrasena = $partes["pass"];
+$basedatos = ltrim($partes["path"], "/");
+$puerto = $partes["port"];
+
+$conexion = new mysqli(
+    $host,
+    $usuario,
+    $contrasena,
+    $basedatos,
+    $puerto
+);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+$conexion->set_charset("utf8");
