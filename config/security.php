c94e4b784ffffb2fdc334bb541ca0f19e04490d7
Romel Molina <romel.molina.est@tecazuay.edu.ec>
2026-08-03 15:52:41 -0500

    Agregar encabezados de seguridad

--- config/security.php
+++ config/security.php
@@ -1,8 +1,11 @@
 <?php
 
+header("X-Frame-Options: DENY");
+header("X-Content-Type-Options: nosniff");
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("X-XSS-Protection: 1; mode=block");
+
 function e($dato)
 {
     return htmlspecialchars($dato, ENT_QUOTES | ENT_HTML5, 'UTF-8');
 }
-
-?>
